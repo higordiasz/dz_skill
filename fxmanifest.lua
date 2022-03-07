c7987f065ade428326a427e02a9d9ba7240b2cd7
Dias <higordiaszuqueto@gmail.com>
2022-03-07 08:10:44 -0300

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Diasz'
+contact 'Discord: Dias#1869'
+version '1.0.0'
+
+ui_page 'nui/ui.html'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'hansolo.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'skywalker.lua'
+}
+
+files {
+	'nui/ui.html',
+	'nui/ui.js',
+    'nui/ui.css',
+    'nui/imagens/*.png',
+}
